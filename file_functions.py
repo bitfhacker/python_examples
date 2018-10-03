948bd43299442add1b4379e268ec6f24864e5633
bitfhacker <bitfhacker@gmail.com>
2018-10-03 15:44:30 +0100

    Update file_functions.py

--- file_functions.py
+++ file_functions.py
@@ -1,2 +1,11 @@
+import os
+
 def getScriptDirPath():
     return os.path.dirname(os.path.realpath(__file__))
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
